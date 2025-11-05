fe0307c5cba6f5b32f875f252b911ea3c0babad2
ismacl <icabanasl22@fpcoruna.afundacion.org>
2025-11-05 11:10:49 +0100

    Backend basico del main para mostrar al usuario y cambio en el fronted

---
+++ Main/main_backend.php
@@ -0,0 +1,14 @@
+<?php 
+require '../Conexion/conexion.php';
+
+//Recupera el ID del usuario desde la sesion
+$id = $_SESSION['id_usuario'];
+
+//Una consulta segura para obtener todos los datos del usuario y que se ejecuta y guarda los resultados en la variable $usuario
+$consulta = $db->prepare("SELECT nombre, apellidos, email, fecha_registro FROM usuarios WHERE id_usuario = ?");
+$consulta -> bind_param("i", $id);
+$consulta -> execute();
+$result = $consulta->get_result();
+$usuario = $result->fetch_assoc();
+$consulta -> close();
+?>
--- Main/main_fronted.php
+++ Main/main_fronted.php
@@ -4,13 +4,16 @@ if (!isset($_SESSION["id_usuario"])) {
     header("Location: ../Login/login_fronted.php");
     exit();
 }
+
+require 'main_backend.php';
 ?>
 
 <html>
     <head><meta charset="UTF-8"><title>Mi perfil</title></head>
 <body>
-    <h1>Bienvenido Usuario <!--<?php echo $usuario['nombre'].''. $usuario['apellidos']; ?>! --></h1>
-    <p>Email: usuario@gmail.com<!--<?php echo $usuario['email'];?>--></p>
+    <h1>Bienvenido, <?php echo $usuario['nombre'].' '. $usuario['apellidos']; ?>!</h1>
+    <p>Email:<?php echo $usuario['email'];?></p>
+    <p>Fecha de registro: <?php echo $usuario['fecha_registro']; ?></p>
 </body>
 
 </html>
