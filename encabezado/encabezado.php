384542ff310c5188fb4b2b2f20d7c2c5e18ff6ec
ismacl <icabanasl22@fpcoruna.afundacion.org>
2025-11-14 12:29:52 +0100

    Creacion del encabezado y de su css

---
+++ encabezado/encabezado.php
@@ -0,0 +1,21 @@
+<html>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/5ab9ff1d93.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="encabezado.css" type="text/css">
+    <header>
+        <div class="logo">
+            <a href="login_fronted.php" title="registro">
+            <img class="logo_bjj" src="../imagenes/logo_bjj_editado.png" alt="logo_bjj">
+            </a>
+        </div>
+        <div class="menu">
+            <a href="#" class="#">Guia de compra</a>
+            <a href="#" class="#">Acerca de nosotros</a>
+            <a href="#" class="#">Contacto</a>
+        </div>
+        <div class="perfil_icono">
+                <a href="../Main/main_fronted.php"><i class="fa-solid fa-user"></i></a>
+            </div>
+        </header>
+</html>
