f9b700818308f922ad0ffae55e2fd348db2582d9
ismacl <icabanasl22@fpcoruna.afundacion.org>
2025-11-17 12:55:58 +0100

    Añadir archivo de footer y su css

---
+++ footer/footer.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="footer.css" type="text/css">
+<script src="https://kit.fontawesome.com/5ab9ff1d93.js" crossorigin="anonymous"></script>
+<footer>
+    <div class="contenido">
+        <div class="info">
+            <p>&copy; 2025 Jiu-Jitsu Zone</p>
+        </div>
+
+        <div class="menu">
+            <a href="#">Política de privacidad</a>
+            <a href="#">Terminos y condiciones</a>
+            <a href="#">Contacto</a>
+        </div>
+
+        <div class="redes">
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+        </div>
+    </div>
+</footer>
